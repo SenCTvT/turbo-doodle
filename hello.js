a1aeb01e31c9fc420788a51230608d6dd822923d
SenCTvT <soumyojitsadhukhan@gmail.com>
2017-10-05 13:37:56 +0530

    this is regex function

--- hello.js
+++ hello.js
@@ -4,13 +4,18 @@ function bar(_r){
 return 5;
 }
 function foo(_r){
-var bar1 = _r(bar);
+
+var bar1 = _r('import bar');
 
 console.log(bar1);
 }
 var _req = function(_request){
+var regex = /import\s([a-z]+)/g;
+var str = _request;
+var m = regex.exec(str);
+var d=eval(m[1]);
 
-return _request(_req);
+return d(_req);
 }
 
 
